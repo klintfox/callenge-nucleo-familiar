995ee43a652b4be6557ae3147409e2af959b552c
klinux <klint_f@hotmail.com>
2019-12-03 13:41:04 -0500

    agregando auditoria

--- src/main/java/com/app/servicio/persona/ServicioPersonaApplication.java
+++ src/main/java/com/app/servicio/persona/ServicioPersonaApplication.java
@@ -3,10 +3,21 @@ package com.app.servicio.persona;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.context.annotation.Bean;
+import org.springframework.data.domain.AuditorAware;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
-@EntityScan({"com.serviciopersonacommon.entity"})
+import com.serviciopersonacommon.audit.AuditorAwareImpl;
+
+@EntityScan({ "com.serviciopersonacommon.entity", "com.serviciopersonacommon.audit" })
+@EnableJpaAuditing(auditorAwareRef = "auditorAware")
 @SpringBootApplication
 public class ServicioPersonaApplication {
+
+	@Bean
+	public AuditorAware<String> auditorAware() {
+		return new AuditorAwareImpl();
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(ServicioPersonaApplication.class, args);
